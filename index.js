7dd9877480319ccae778f91e0530b5495a72bc0d
Russell Hay <me@russellhay.com>
2016-05-30 08:56:18 -0700

    Initial Checkin with rainbow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+var rainbow = require('./lib/rainbow');
+// var gradient = require('./lib/gradient');
+
+module.exports = {
+  fill_rainbow: rainbow
+};
---
+++ test/rainbow.js
@@ -0,0 +1,35 @@
+require('chai').should();
+var expect = require('chai').expect;
+var fill_rainbow = require('../index').fill_rainbow;
+
+describe('fill_rainbow', () => {
+  it('generates a gradient', () => {
+    var arr = new Array(3);
+    fill_rainbow(arr, 0, 45);
+    arr[0].should.equal('#ff0000');
+    arr[1].should.equal('#ffbf00');
+    arr[2].should.equal('#80ff00');
+  });
+
+  it('can fill only a portion of the array', () => {
+    var arr = new Array(5);
+    arr[0] = '#000000';
+    arr[4] = '#000000';
+    fill_rainbow(arr, 0, 45, 1, 3);
+    arr[0].should.equal('#000000');
+    arr[1].should.equal('#ff0000');
+    arr[2].should.equal('#ffbf00');
+    arr[3].should.equal('#80ff00');
+    arr[4].should.equal('#000000');
+  });
+
+  it('does not fail if passed not an array', () => {
+    var fn = () => {
+      var foo = 5;
+      fill_rainbow(foo, 10, 10, 1, 2);
+      foo.should.equal(5);
+    };
+
+    expect(fn).to.not.throw();
+  });
+});
